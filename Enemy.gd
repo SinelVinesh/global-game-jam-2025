659b24095be2c7fe842fe3411de17461ac5c9cd6
SinelVinesh <vinesh.sinel@gmail.com>
2025-01-22 08:59:41 +0300

    Init project

--- Enemy.gd
+++ Enemy.gd
@@ -1,3 +1,7 @@
-extends KinematicBody2D
-func _ready(): position = get_parent().get_node("Player").position + Vector2(1000, 0).rotated(rand_range(0, 2*PI))
-func _physics_process(delta): move_and_slide((get_parent().get_node("Player").position - position).normalized() * $AnimatedSprite.speed_scale / delta)
+extends CharacterBody2D
+func _ready(): 
+	position = get_parent().get_node("Player").position + Vector2(1000, 0).rotated(randf_range(0, 2*PI))
+	set_velocity((get_parent().get_node("Player").position - position).normalized() * $AnimatedSprite2D.speed_scale / delta)
+	move_and_slide()
+func _physics_process(delta): 
+	velocity
