3bbabcff653d1e935b2baf771feffc5554df41a2
NoobNekoXD <volahary7895123@gmail.com>
2025-01-22 18:53:49 +0300

    Migrate project to Godot 4

--- Enemy.gd
+++ Enemy.gd
@@ -1,7 +1,27 @@
 extends CharacterBody2D
-func _ready(): 
-	position = get_parent().get_node("Player").position + Vector2(1000, 0).rotated(randf_range(0, 2*PI))
-	set_velocity((get_parent().get_node("Player").position - position).normalized() * $AnimatedSprite2D.speed_scale / delta)
+
+
+@export var speed = 100
+
+
+var player_position
+
+
+func _ready():
+	player_position = get_parent().get_node("Player").global_position
+	position = player_position + Vector2(1000, 0).rotated(randf_range(0, 2 * PI))
+	velocity = (player_position - position).normalized() * speed
+
+
+func _physics_process(delta):
+	player_position = get_parent().get_node("Player").global_position
+	velocity = (player_position - position).normalized() * speed
 	move_and_slide()
-func _physics_process(delta): 
-	velocity
+
+	if velocity != Vector2.ZERO:
+		$AnimatedSprite2D.play("default")
+
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.flip_h = false
