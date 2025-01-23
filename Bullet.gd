5ac41fa51271ed382504782ea1e715bbcc56be5d
NoobNekoXD <volahary7895123@gmail.com>
2025-01-23 16:43:50 +0300

    hmmmmmmmm
    aaaaaaaaahhhhh

--- Bullet.gd
+++ Bullet.gd
@@ -1,2 +1,10 @@
 extends Node2D
-func enemyHit(area: Area2D): area.get_parent().get_node("AnimationPlayer").play("Death")
+
+
+#Handle hit ennemy
+func enemyHit(area: Area2D):
+	var ennemy_hit = area.get_parent()
+	ennemy_hit.get_node("AnimationPlayer").play("Death")
+	var player_owner = get_parent()
+	var experience_gained = ennemy_hit.experience_point
+	player_owner.get_node("Experience_Player")._gain_experience(experience_gained)
---
+++ Experience_player.gd
@@ -0,0 +1,40 @@
+extends Control
+
+
+@export var level = 1
+
+var experience = 0
+var experience_required = _get_experience_required(level + 1)
+var amount
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	$Label_Experience.text = str(experience)
+	$Label_Experience_required.text = str(experience_required)
+	$Label_Level.text = str (level)
+
+
+#Get random experience required to level up
+func _get_experience_required(level):
+	return round (pow (level, 3) + level * 50)
+
+
+#Handle experience gaining
+func _gain_experience(amount):
+	experience += amount
+	while experience >= experience_required:
+		experience -= experience_required
+		_level_up()
+
+
+#Handle leveling up
+func _level_up():
+	level += 1
+	experience_required = _get_experience_required(level + 1)
